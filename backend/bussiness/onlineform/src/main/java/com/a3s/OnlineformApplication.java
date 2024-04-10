ec36cc3cc44fdeccd6b0414cfa2183d17f1d00e7
vikrant <vikrantrathore09@gmail.com>
2024-04-10 20:18:20 +0530

    initial commit

---
+++ backend/bussiness/onlineform/src/main/java/com/a3s/OnlineformApplication.java
@@ -0,0 +1,13 @@
+package com.a3s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineformApplication.class, args);
+	}
+
+}
---
+++ backend/bussiness/onlineform/src/test/java/com/a3s/OnlineformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
